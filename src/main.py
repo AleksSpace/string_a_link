27593171dd63fcc6e927b605900c861be1f8bd37
Алексей <Zaikin.lescha@yandex.ru>
2022-11-18 16:35:55 +0300

    Create test

--- src/main.py
+++ src/main.py
@@ -1,19 +1,23 @@
 import requests
+from requests.exceptions import InvalidURL, ConnectionError, MissingSchema
 import json
 
 
 def url_or_string(link):
     """Функция определяет является строка ссылкой"""
+    FAILED_MESSAGE = f"Строка '{link}' не является ссылкой"
+
     try:
         response = requests.get(link)
         if "text/html" in response.headers["Content-Type"]:
             return True
-        else:
-            return False
-    except requests.ConnectionError as exception:
-        print(f"{exception}: Строка '{link}' не является ссылкой")
-    except requests.exceptions.InvalidURL as exception:
-        print(f"{exception}: Строка '{link}' не является ссылкой")
+        return False
+    except ConnectionError:
+        print(f"{FAILED_MESSAGE}")
+    except InvalidURL:
+        print(f"{FAILED_MESSAGE}")
+    except MissingSchema:
+        print(f"{FAILED_MESSAGE}")
 
 
 def get_methods(string_list):
@@ -21,8 +25,9 @@ def get_methods(string_list):
     и возвращает json
     """
     result = {}
-    ctat_code = {}
+    
     for link in string_list:
+        ctat_code = {}
         if url_or_string(link):
             get = requests.get(link).status_code
             if get != 405:
@@ -30,28 +35,22 @@ def get_methods(string_list):
                 result[link] = ctat_code
             head = requests.head(link).status_code
             if head != 405:
-                ctat_code["HEAD"] = head
-                result[link] = ctat_code
+                result[link]["HEAD"] = head
             post = requests.post(link).status_code
             if post != 405:
-                ctat_code["POST"] = post
-                result[link] = ctat_code
+                result[link]["POST"] = post
             put = requests.put(link).status_code
             if put != 405:
-                ctat_code["PUT"] = put
-                result[link] = ctat_code
+                result[link]["PUT"] = put
             delete = requests.delete(link).status_code
             if delete != 405:
-                ctat_code["DELETE"] = delete
-                result[link] = ctat_code
+                result[link]["DELETE"] = delete
             options = requests.options(link).status_code
             if options != 405:
-                ctat_code["OPTIONS"] = options
-                result[link] = ctat_code
+                result[link]["OPTIONS"] = options
             patch = requests.patch(link).status_code
             if patch != 405:
-                ctat_code["PATCH"] = patch
-                result[link] = ctat_code
+                result[link]["PATCH"] = patch
     return json.dumps(result, indent=4)
 
 
@@ -60,5 +59,6 @@ if __name__ == "__main__":
                    "https://youtube.com",
                    "Какая то строка",
                    "http://grkdvjktu.com",
+                   "google.com",
                    "http://yandex.ru"]
     print(get_methods(string_list))
--- tests/test_url.py
+++ tests/test_url.py
@@ -1,4 +1,8 @@
 import requests
+from  requests.exceptions import InvalidURL, ConnectionError
+import pytest
+import json
+
 
 from src.main import url_or_string, get_methods
 
@@ -6,15 +10,57 @@ LIST_STRING = [
    "https://google.com",
    "Какая то строка",
    "http://grkdvjktu.com",
-   "http://yandex.ru"
+   "google.com",
+   "https://ru.wikipedia.org/api/rest_v1/page/pdf/Заглавная_страница"
 ]
 
 def test_string_is_a_link():
-    # response = requests.get(url=LIST_STRING[0])
     assert url_or_string(LIST_STRING[0]) == True
-    assert url_or_string(LIST_STRING[1]) == True
-    assert url_or_string(LIST_STRING[2]) == True
-    assert url_or_string(LIST_STRING[3]) == True
+
+    assert url_or_string(LIST_STRING[1]) == None
+
+    assert url_or_string(LIST_STRING[2]) == None
+
+    assert url_or_string(LIST_STRING[3]) == None
+
+    assert url_or_string(LIST_STRING[4]) == False
 
 
+def test_get_method():
+   list_url = ["https://google.com", "https://youtube.com", "http://yandex.ru"]
+   result_dict = {
+       "https://google.com": { 
+           "GET": 200,
+           "HEAD": 301
+       },
+       "https://youtube.com": {
+           "GET": 200,
+           "HEAD": 301,
+           "POST": 400,
+           "PUT": 400
+       },
+       "http://yandex.ru": {
+           "GET": 200,
+           "HEAD": 302,
+           "POST": 200,
+           "PUT": 200,
+           "DELETE": 200,
+           "OPTIONS": 200,
+           "PATCH": 200
+       }
+   }
+   print(get_methods(list_url))
+   assert get_methods(list_url) == json.dumps(result_dict, indent=4)
 
+def test_except_massege(capfd):
+    get_methods([LIST_STRING[1]])
+    out, err = capfd.readouterr()
+    assert out == f"Строка '{LIST_STRING[1]}' не является ссылкой\n"
+
+    get_methods([LIST_STRING[2]])
+    out, err = capfd.readouterr()
+    assert out == f"Строка '{LIST_STRING[2]}' не является ссылкой\n"
+
+    get_methods([LIST_STRING[3]])
+    out, err = capfd.readouterr()
+    assert out == f"Строка '{LIST_STRING[3]}' не является ссылкой\n"
